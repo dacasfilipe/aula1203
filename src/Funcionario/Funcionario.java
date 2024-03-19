04f43b9ee5f52a3a440d7c43e84e7cc1396604c7
dacasfilipe <filiperdacas@gmail.com>
2024-03-19 19:10:27 -0300

    add funcionario

---
+++ src/Funcionario/Funcionario.java
@@ -0,0 +1,13 @@
+package Funcionario;
+
+public class Funcionario {
+    String nome;
+    String departamento;
+    double salario;//1000
+
+    public void aumentarSalario(double percentual){
+        double aumento = this.salario * (percentual/100);
+        this.salario = this.salario + aumento;
+        System.out.println("O novo Salário é:"+this.salario);
+    }
+}
