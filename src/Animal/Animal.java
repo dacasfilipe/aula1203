7c4307b4276593e1c50c7d7203d311f97b10c6ea
dacasfilipe <filiperdacas@gmail.com>
2024-03-19 19:45:31 -0300

    add Animal

---
+++ src/Animal/Animal.java
@@ -0,0 +1,10 @@
+package Animal;
+
+public class Animal {
+    String nome;
+    String tipo;
+    int idade;
+    public void emitirSom(String som){
+        System.out.println("O animal faz "+som);
+    }
+}
