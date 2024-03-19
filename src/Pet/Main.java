bd890e3143fbe427e21a38fb5145daad3af46810
dacasfilipe <filiperdacas@gmail.com>
2024-03-19 19:41:08 -0300

    finished Pet

---
+++ src/Pet/Main.java
@@ -0,0 +1,11 @@
+package Pet;
+
+public class Main {
+    public static void main(String[] args) {
+        Pet pet = new Pet();
+        pet.nome = "Ana";
+        pet.especie = "Border Collie";
+        pet.idade = 1;
+        pet.exibir();
+    }
+}
