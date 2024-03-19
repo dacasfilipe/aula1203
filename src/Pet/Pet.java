2e782a7f83915ecd0d1925db2241c46e2f454677
dacasfilipe <filiperdacas@gmail.com>
2024-03-19 19:37:12 -0300

    add Pet

---
+++ src/Pet/Pet.java
@@ -0,0 +1,12 @@
+package Pet;
+
+public class Pet {
+    String nome;
+    String especie;
+    int idade;
+    public void exibir(){
+        System.out.println("Nome:"+this.nome);
+        System.out.println("Espécie:"+this.especie);
+        System.out.println("Idade:"+this.idade);
+    }
+}
