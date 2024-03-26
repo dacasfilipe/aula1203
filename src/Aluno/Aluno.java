4d0c1c9e0ed9f689828faf02e8575250fbedf2f2
dacasfilipe <filiperdacas@gmail.com>
2024-03-26 19:40:52 -0300

    add Aluno.java

---
+++ src/Aluno/Aluno.java
@@ -0,0 +1,16 @@
+package Aluno;
+
+public class Aluno {
+    String nome;
+    String matricula;
+    String curso;
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", curso='" + curso + '\'' +
+                '}';
+    }
+}
