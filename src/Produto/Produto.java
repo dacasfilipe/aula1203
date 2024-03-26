12c6a901a37621edf86649f0cec8e734d53f87ef
dacasfilipe <filiperdacas@gmail.com>
2024-03-26 20:00:31 -0300

    add Produto.java

---
+++ src/Produto/Produto.java
@@ -0,0 +1,11 @@
+package Produto;
+
+public class Produto {
+    String nome;
+    double preco;
+    int quantidadeEmEstoque;
+
+    public double calcularValorTotalEstoque(){
+        return this.preco * this.quantidadeEmEstoque;
+    }
+}
