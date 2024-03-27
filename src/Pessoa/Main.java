f6db025cc7808153ff7a2f98d1d0b664c0456ff5
dacasfilipe <filiperdacas@gmail.com>
2024-03-26 21:05:26 -0300

    add Pessoa.java

--- src/Pessoa/Main.java
+++ src/Pessoa/Main.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        Pessoa pessoa1 = new Pessoa("thor",200);
-        pessoa1.setNome("Gilberto");
-        //informar o nome da pessoa usando
-        System.out.println("Nome:"+pessoa1.getNome());
-        System.out.println("Digite o novo nome:");
-        String novoNome = leia.next();
-        pessoa1.setNome(novoNome);
-        System.out.println("Nome atualizado:"+pessoa1.getNome());
-        pessoa1.setIdade(350);
-        System.out.println("Idade:"+pessoa1.getIdade());
+        System.out.println("Informe o nome:");
+        String nome = leia.next();
+        System.out.println("Informe a idade:");
+        int idade = leia.nextInt();
+        System.out.println("CPF:");
+        String cpf = leia.next();
+        Pessoa pessoa1 = new Pessoa(nome,idade,cpf);
+        System.out.println(pessoa1.toString());
 
     }
 }
--- src/Pessoa/Pessoa.java
+++ src/Pessoa/Pessoa.java
@@ -3,11 +3,14 @@ package Pessoa;
 public class Pessoa {
     private String nome;
     private int idade;
+    private String cpf;
     //construtor
 
-    public Pessoa(String nome, int idade) {
+
+    public Pessoa(String nome, int idade, String cpf) {
         this.nome = nome;
         this.idade = idade;
+        this.cpf = cpf;
     }
 
     public String getNome() {
@@ -26,5 +29,20 @@ public class Pessoa {
         this.idade = idade;
     }
 
+    public String getCpf() {
+        return cpf;
+    }
 
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
 }
